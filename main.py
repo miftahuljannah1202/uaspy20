29bac3c9389e83a73f2ea3bc4cf4bd70aa7db9fa
miftahuljannah <miftahuljannah2646@gmail.com>
2020-01-13 11:45:28 +0700

    update

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
+from core.baseapp import BaseApp
+from core.search_helper import SearchHelper
+from data_model import book
+from data_model.author import Author
+from data_model.publication import Publication
+
+
 class MainApp():
     def __init__(self):
         self.books = []
+        self.InputBook = []
+        self.ViewBook = []
+        BaseApp.__init__(self)
+
+class ViewBook(Book):
+    def init (self):
+        vBook = ViewBook
+        vBook.list_book()
 
 
 if __name__ == "__main__":
     app = MainApp()
     app.run()
 
+    @property
+    def list_book(self):
+        return
+
+    def add_book(self):
+        return self.add_book()
+
+    def search_book(self):
+        return self.search_book()
